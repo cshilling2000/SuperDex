52730af5386d17b2b33f4f7c24a85e4f8db5835e
Caleb Shilling <shillinc@oregonstate.edu>
2023-10-10 16:26:29 -0700

    added comments and more dex helper functions, removed tauros forms as they will be hard coded

--- dex_processor.py
+++ dex_processor.py
@@ -3,6 +3,7 @@ import csv
 dex_filename = "natdex.csv"
 alolan_forms_filename = "forms\\alola.csv"
 
+# Pull the national dex from dex_filename.
 def grab_names_and_numbers():
     names = []
     with open(dex_filename) as csvfile:
@@ -11,12 +12,15 @@ def grab_names_and_numbers():
     
     return names
 
+# Gets a dex number from a Pokemon name.
 def get_dex_no_from_name(names, name):
     return names.index(name) + 1
 
+# Gets the name of a Pokemon from it's number.
 def get_name_from_no(names, no):
     return names[no-1]
 
+# Gets forms from a single file (excluding Tauros forms)
 def get_forms(forms_filename):
     numbers = []
     with open(forms_filename) as csvfile:
@@ -25,11 +29,18 @@ def get_forms(forms_filename):
  
     return numbers
 
+# Converts the national dex to a list of numbers.
 def name_list_to_no_list(names):
     return range(1, len(names) + 1)
 
-def form_mask(parent,mask):
+# Masks a list of numbers out of another list.
+def number_mask(parent,mask):
     for element in mask:
         parent.remove(element)
 
-
+# Adds an identifier to names: ex, "Alolan Raticate."
+def form_name_maker(names,form_nos,form_name):
+    form_names = []
+    for no in form_nos:
+        form_names.append(form_name + ' ' + get_name_from_no(names,no))
+    return form_names
